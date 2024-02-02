6272d3718c31cdd84d1a4f85caf50ce8ad2ceecc
Martin Orsulik <martin.orsulik@cz.ey.com>
2024-02-02 13:07:04 +0000

    added delivery_team seed and added to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
 -- from raw orders
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders, -- surrogate key
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -8,7 +9,6 @@ o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordercostprice,
 o.ordersellingprice,
 -- from raw customer
-{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders, -- surrogate key
 c.customername,
 c.customerid,
 c.segment,
@@ -19,9 +19,11 @@ p.productname,
 p.productid,
 p.subcategory,
 {{ markup('ordersellingprice', 'ordercostprice') }} as markup, -- tohle je markup první způsob
-{{ markup_v2() }} as markup_v2 -- tohle je makro ze souboru markup_v2.sql druhý způsob
+{{ markup_v2() }} as markup_v2, -- tohle je makro ze souboru markup_v2.sql druhý způsob
+d.delivery_team
 from {{ ref('raw_orders') }} as o -- dbt jinja funkce reference
 left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d on o.shipmode = d.shipmode
 
 {{limit_data_in_dev('orderdate')}} -- nastavení limitu v tomto scriptu, to je column_name z macra
